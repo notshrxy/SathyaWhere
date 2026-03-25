e74f85aec8d60e971572ffb7de946df1ae018198
Shreyas <shreyasofficial2904@gmail.com>
2026-03-25 23:21:09 +0530

    CSS Process Plugins specified

---
+++ ts/postcss.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * postcss.config.mjs
+ * PostCSS configuration file. It specifies the plugins used to process
+ * CSS, such as the Tailwind CSS PostCSS plugin.
+ */
+
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
