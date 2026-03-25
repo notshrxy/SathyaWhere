711105574abe0a93bc274da493dccdcf4f533ef3
Shreyas <shreyasofficial2904@gmail.com>
2026-03-25 23:17:05 +0530

    Main config for Next.js for custom build

---
+++ ts/next.config.ts
@@ -0,0 +1,14 @@
+/**
+ * next.config.ts
+ * Main configuration file for Next.js. Used to define custom build settings,
+ * environment variables, and framework behavior like React Strict Mode.
+ */
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+export default nextConfig;
