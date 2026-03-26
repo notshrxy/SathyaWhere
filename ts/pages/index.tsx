81cbea2bb3898597bf8cb53a6c88ea2ad85af730
Shreyas <shreyasofficial2904@gmail.com>
2026-03-26 19:48:42 +0530

    Root index that renders LandingPage compnent

---
+++ ts/pages/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * pages/index.tsx
+ * The root index page of the application.
+ * Currently acts as a wrapper that renders the main `LandingPage` component.
+ */
+
+import LandingPage from './LandingPage';
+
+export default function Home() {
+  return <LandingPage />;
+}
